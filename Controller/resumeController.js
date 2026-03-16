3e9b60bc09b3062034ba4ae6caee705557c00a17
mailtotechstack <mailtotechstack@gmail.com>
2026-03-16 11:01:00 +0530

    get and post route defined

--- Controller/resumeController.js
+++ Controller/resumeController.js
@@ -1,9 +1,21 @@
 import mongoose from "mongoose";
+import { Resume } from "../Models/Resume";
 
 export const getAllData = async (req, res)=>{
-    res.send("get Requested");
+    const data = await Resume.find({});
+    if(!data) return res.status(404).json({message: "Data not found"});
+    return res.status(200).json(
+        {
+            message : "Data Fetched Sucessfully",
+            data: data
+        }
+    );
 }
 
 export const setAllData = async (req, res)=>{
-    res.send(req.body);
-}
+    const newResume = new Resume(req.body, {new: true});
+    newResume.save();
+    res.status(200).json({
+        message : "Data saved successfully",
+    });
+};
